3281471253e7a41fac4ff3c4cd7e4bc4316b6ec6
Calel95 <borgescalel@gmail.com>
2024-09-06 19:21:12 -0300

    retirando os testes que nao deu certo

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from typing import Generator
-import pytest
-from src.main import app
-
-@pytest.fixture(scope="function")
-def client() -> Generator:
-    with TestClient(app) as c:
-        yield c
